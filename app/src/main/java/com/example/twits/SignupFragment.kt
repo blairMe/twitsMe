34203329e19266dafed8e55b3c98df1701ffa49a
blairMe <felixanachoni@gmail.com>
2022-10-04 23:43:06 +0300

    simple twitter

--- app/src/main/java/com/example/twits/SignupFragment.kt
+++ app/src/main/java/com/example/twits/SignupFragment.kt
@@ -2,11 +2,13 @@ package com.example.twits
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.ThemedSpinnerAdapter.Helper
 import androidx.navigation.Navigation
 import com.blair.twits.R
 import com.blair.twits.databinding.FragmentSignupBinding
@@ -14,6 +16,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.regex.Pattern.matches
 
 class SignupFragment : Fragment() {
 
@@ -51,12 +54,12 @@ class SignupFragment : Fragment() {
         }
 
         signupUser()
-
-
-
     }
 
     private fun signupUser() {
+
+        userDetailsFocusedListener()
+
 
         binding.btnSignup.setOnClickListener {
             val firstName = binding.userFirstName.text.toString()
@@ -90,6 +93,8 @@ class SignupFragment : Fragment() {
         }
     }
 
+
+
     private fun sendDataToFireStore(firstName : String, secondName : String, email : String, password : String) {
         // Capture the user information
         val userInfo = hashMapOf(
@@ -111,6 +116,43 @@ class SignupFragment : Fragment() {
                     Toast.LENGTH_SHORT).show()
             }
 
+
+    }
+
+    // Validation
+    private fun userDetailsFocusedListener() {
+        binding.userFirstName.setOnFocusChangeListener { _, _ ->
+            val firstNameText = binding.userFirstName.text.toString()
+            if(!firstNameText.matches(".*[a-z].*".toRegex())) {
+                binding.firstNameContainer.helperText = "Must be characters"
+            } else {
+                binding.firstNameContainer.helperText = ""
+            }
+        }
+        binding.userSecondName.setOnFocusChangeListener { _, _ ->
+            val secondNameText = binding.userSecondName.text.toString()
+            if(!secondNameText.matches(".*[a-z].*".toRegex())) {
+                binding.secondNameContainer.helperText = "Must be characters"
+            } else {
+                binding.secondNameContainer.helperText = ""
+            }
+        }
+        binding.userEmail.setOnFocusChangeListener { _, _ ->
+            val emailText = binding.userEmail.text.toString()
+            if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+                binding.emailContainer.helperText = "Invalid email address"
+            } else {
+                binding.emailContainer.helperText = ""
+            }
+        }
+        binding.userPassword.setOnFocusChangeListener { _, _ ->
+            val password = binding.userPassword.text.toString()
+            if(password.length < 8) {
+                binding.passwordContainer.helperText = "Password must be atleast 8 characters"
+            } else if (!password.matches(".*[a-z].*".toRegex())) {
+                binding.passwordContainer.helperText = "Password must contain atleast one lowercase letter"
+            }
+        }
 
     }
 }
