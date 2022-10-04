6702cf5b246da460300e1920ce3b989148007108
blairMe <felixanachoni@gmail.com>
2022-10-04 17:09:35 +0300

    simple twitter

--- app/src/main/java/com/example/twits/SignupFragment.kt
+++ app/src/main/java/com/example/twits/SignupFragment.kt
@@ -1,25 +1,25 @@
 package com.example.twits
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.blair.twits.R
 import com.blair.twits.databinding.FragmentSignupBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignupFragment : Fragment() {
 
-    private lateinit var auth: FirebaseAuth
-
     private lateinit var binding: FragmentSignupBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var auth: FirebaseAuth = Firebase.auth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,12 +30,43 @@ class SignupFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.toSignin.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_signupFragment_to_signinFragment)
         }
+
+        binding.btnSignup.setOnClickListener {
+            val email = binding.userEmail.text.toString()
+            val password = binding.userPassword.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Successful signup
+                        Toast.makeText(requireActivity(), "Successful signing in",
+                            Toast.LENGTH_SHORT).show()
+                        val intent = Intent(activity, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // Unsuccessful signup fails
+                        Toast.makeText(requireActivity(), "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
 
 
 
