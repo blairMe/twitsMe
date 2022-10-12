02dedb0467105ce1ec203a6f972b53b0cfdbc140
blairMe <felixanachoni@gmail.com>
2022-10-12 18:26:08 +0300

    simple twitter

--- app/src/main/java/com/blair/twits/ui/activities/MainActivity.kt
+++ app/src/main/java/com/blair/twits/ui/activities/MainActivity.kt
@@ -1,28 +1,23 @@
 package com.blair.twits.ui.activities
 
-import android.app.Activity
-import android.content.ContextWrapper
-import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.blair.twits.R
 import com.blair.twits.databinding.ActivityMainBinding
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.OutputStream
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var theButton = binding.firstText
-        theButton.text = "Bye Hello"
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        setupActionBarWithNavController(navController, null)
     }
 
 }
