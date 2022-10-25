f89645d3d3be803a51fc8592bd2aa3a3cc08e01c
blairMe <felixanachoni@gmail.com>
2022-10-25 23:25:04 +0300

    simple twitter

--- app/src/main/java/com/blair/twits/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/blair/twits/ui/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.blair.twits.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +10,13 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.blair.twits.R
 import com.blair.twits.databinding.FragmentHomeBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
+
+    // Firestore db
+    val db = Firebase.firestore
 
     private lateinit var binding : FragmentHomeBinding
 
@@ -35,6 +41,31 @@ class HomeFragment : Fragment() {
                 .navigate(R.id.action_homeFragment_to_postTwitFragment)
         }
 
+        getTwits()
+
+
+    }
+
+    fun getTwits() : ArrayList<String> {
+        val twitNamesArray =  ArrayList<String>()
+
+
+        db.collection("twits")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    // Log.d("Twit data", "${document.id} => ${document.data}")
+                    // Log.i("Twit data", "${document.data}")
+                    for(fileName in document.id) {
+                        twitNamesArray.add(fileName.toString())
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("Twit data", "Error getting documents: ", exception)
+            }
+
+        return twitNamesArray
 
     }
 
