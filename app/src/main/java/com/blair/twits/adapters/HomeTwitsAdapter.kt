dbb32d0cba0a2592b285f6bc9011b11a467847f0
blairMe <felixanachoni@gmail.com>
2022-11-08 00:56:48 +0300

    simple twitter

--- app/src/main/java/com/blair/twits/adapters/HomeTwitsAdapter.kt
+++ app/src/main/java/com/blair/twits/adapters/HomeTwitsAdapter.kt
@@ -38,8 +38,6 @@ class HomeTwitsAdapter(private val fragment : Fragment, private val twitsArray :
         //Log.i("Array Items", "$twitItems")
         val docRef = db.collection("twits").document(twitItems)
 
-        Log.i("The Items", "$twitItems")
-
         // Get the document
         docRef.get()
             .addOnCompleteListener { task ->
